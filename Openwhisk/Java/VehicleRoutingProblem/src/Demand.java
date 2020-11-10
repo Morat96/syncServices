432ffc9ad26d8a536b1e33e53152490d7abfcb7d
Morat96 <moratellomatteo@libero.it>
2020-11-10 18:36:48 +0100

    Added action VRP for Java

---
+++ Openwhisk/Java/VehicleRoutingProblem/src/Demand.java
@@ -0,0 +1,4 @@
+public class Demand {
+
+    int value;
+}
---
+++ Openwhisk/Java/VehicleRoutingProblem/src/Solution.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+
+public class Solution implements Cloneable {
+
+    double cost;
+    ArrayList<Route> routes;
+
+    Solution ()
+    {
+        routes = new ArrayList<Route>();
+        cost = 0;
+    }
+
+    public Object clone() throws
+            CloneNotSupportedException
+    {
+        return super.clone();
+    }
+}
